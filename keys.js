8e74be7c7be562e000daa003bcec429af049d011
densk1 <>
2018-02-13 14:27:47 +0000

    Installed SASS successfully.

---
+++ keys.js
@@ -0,0 +1,6 @@
+const dbuser = '***REMOVED***';
+const dbpassword = '***REMOVED***';
+
+module.exports ={ 
+    mongoURI: `mongodb://***REMOVED***:***REMOVED***@ds1***REMOVED***.mlab.com:***REMOVED***/react-crm`
+}
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
 	Route, 
 	Switch,
 } from 'react-router-dom';
-//import './styles/css/index.css';
+import './styles/index.css';
 
 import NavBar from './components/Navbar';
 
