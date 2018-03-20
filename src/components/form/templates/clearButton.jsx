abbfada2d2a7c9e8a45556dec8844324b52252f7
densk1 <cardenk@gmail.com>
2018-03-20 01:11:47 +0000

    Refactoring clearButton component.

--- src/components/form/templates/clearButton.jsx
+++ src/components/form/templates/clearButton.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
-const clearButton = (props) => (
-	<div>
-		<button 
-			className={"btn  w-100 "+(props.clearDisabled? "btn-outline-secondary" : "btn-outline-danger" )} 
-			type="submit" 
-			disabled={props.clearDisabled}
-			onClick={props.onClick}
-		>
-			{props.text}
-		</button>
-	</div>
-)
+const clearButton = ({ clearDisabled, onClick, text }) => (
+  <div>
+    <button
+      className={`btn w-100 ${(clearDisabled ? 'btn-outline-secondary' : 'btn-outline-danger')}`}
+      type="submit"
+      disabled={clearDisabled}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  </div>
+);
 
 export default clearButton;
 
 
 /*
-				<Field 
-					name={'clear'}
-					component={clearButton} 
-					clearDisabled={pristine || submitting}
-					onClick={reset}
-					/>
+<Field
+name="clear"
+component={clearButton}
+clearDisabled={pristine || submitting}
+onClick={reset}
+/>
 
 */
